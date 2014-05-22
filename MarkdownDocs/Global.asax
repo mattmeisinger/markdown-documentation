a40d003abd8cc1fee0a599cb3ddb91a00a47b93b
Matthew Meisinger <mrm2205@columbia.edu>
2014-05-22 07:36:39 -0400

    Initial Commit - App Framework

---
+++ MarkdownDocs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarkdownDocs.MvcApplication" Language="C#" %>
---
+++ MarkdownDocs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarkdownDocs.Startup))]
+namespace MarkdownDocs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
